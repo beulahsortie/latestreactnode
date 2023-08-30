2fca860d8315c6acd468bd737721d60ab90a9573
beulahsortie <3231811912y204@gmail.com>
2023-08-30 07:02:35 +0530

    Initial commit: Add React app files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    name: ""
+  }
+
+  componentDidMount() {
+    
+
+    fetch("http://localhost:3000/first")
+      .then(res => { 
+        console.log('Response:', res)
+      return res.json();})
+      .then(data => {
+        console.log('data:', data)
+        this.setState({ name: data.name })})
+
+      console.log(this.state.name);
+  }
+
+  render() {
+    return (
+      
+      <h1>Hello {this.state.name}!</h1>
+    )
+  }
 }
 
-export default App;
+export default App
+
